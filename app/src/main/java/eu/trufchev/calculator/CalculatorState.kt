6bbcf6050c345f0c6f7d2249f92eee087c3a63e1
ytrufchev <yordan.tihomirov@gmail.com>
2024-02-19 13:44:54 +0200

    Initial commit

---
+++ app/src/main/java/eu/trufchev/calculator/CalculatorState.kt
@@ -0,0 +1,7 @@
+package eu.trufchev.calculator
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
---
+++ app/src/main/java/eu/trufchev/calculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package eu.trufchev.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import eu.trufchev.calculator.ui.theme.CalculatorTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            eu.trufchev.calculator.ui.theme.CalculatorTheme {
+                val viewModel = androidx.lifecycle.viewmodel.compose.viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(androidx.compose.ui.graphics.Color.Companion.DarkGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
